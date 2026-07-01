315f501946b4afe028f3ecd900f3e3f37886ef99
Mahesh Bhatt <mohanbhatt805777@gmail.com>
2026-07-01 18:02:51 +0530

    added login files for user

---
+++ customer_pannel/index.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+
+
+const port=8080;
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+
+app.listen(port,()=>{
+  console.log("Server is running at port "+port);
+});
+
+app.get("/",(req,res)=>{
+  res.send("Server is running");
+})
+
+app.get("/login",(req,res)=>{
+  res.render("loginpage.ejs");
+});
+app.get("/signup",(req,res)=>{
+  res.render("signUppage.ejs");
+});
+
